144c115f5df9fc19ee7c489a639778680773fce1
qorbaxk <bekgyber3@hanmail.net>
2022-05-31 20:02:50 +0900

    finish

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -9,7 +9,7 @@ const ProductDetail = () => {
   const [product, setProduct] = useState(null);
 
   const getProductDetail = async () => {
-    let url = `http://localhost:5000/products/${id}`;
+    let url = `https://my-json-server.typicode.com/qorbaxk/H-M_Homepage/products/${id}`;
     let response = await fetch(url);
     let data = await response.json();
     setProduct(data);
